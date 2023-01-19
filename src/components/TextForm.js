561dc5e74a4a8aeac9491805e833bc68f9d0a750
Shailendra Kushwaha <learning4succedding@gmail.com>
2023-01-19 03:33:09 +0200

    textutils project started

---
+++ src/components/TextForm.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+  const handleUpclick = () =>{
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+  const handleLoclick = () =>{
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  const handleOnChange = (event) =>{
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState(' ');
+  return (
+    <>
+    <div className='container'>
+      <h1>{props.heading} </h1>
+       <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8">  </textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoclick}>Convert to Lowercase</button>
+    </div>
+    <div className="container">
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words and {text.length} chracters</p>
+      <p>{0.008 * text.split(" ").length } minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
+  )
+}
